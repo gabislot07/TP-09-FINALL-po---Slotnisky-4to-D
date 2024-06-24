4e46566497dc7e7a18c1d7c8fc5b86eb48a1c632
gabislot07 <144163994+gabislot07@users.noreply.github.com>
2024-06-24 09:15:34 -0300

    Initial commit

---
+++ Assets/Controlador.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    int indice = -1;
+    public GameObject[] objetos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ALL_desactivate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Pasar();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            atras();
+        }
+    }
+
+    void ALL_desactivate()
+    {
+        foreach (GameObject objeto in objetos)
+        {
+            objeto.gameObject.SetActive(false);
+        }
+    }
+
+    void Pasar()
+    {
+        ALL_desactivate();
+        indice++;
+        if (indice == objetos.Length)
+        {
+            indice = 0;
+        }
+        objetos[indice].gameObject.SetActive(true);
+    }
+    void atras()
+    {
+        ALL_desactivate();
+        indice--;
+        if (indice < 0)
+        {
+            indice = objetos.Length - 1;
+        }
+        objetos[indice].gameObject.SetActive(true);
+    }
+}
